d9816d365019266be1eda72dc89666a9716794c1
cookel2 <Eleanor.Cook@ons.gov.uk>
2018-11-15 11:57:49 +0000

    initial code from flask tutorial and my work on the iac stub

---
+++ app.py
@@ -0,0 +1,20 @@
+from structlog import PrintLogger #added by EC
+
+from flask import Flask
+from iac_stub import Iac_Stub
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+	return "Hello again from Census IAC Stub!"
+
+@app.route('/iacs/b4t7g3xby5bx')
+def show_iac_data():
+	PrintLogger().info("Now in the show_iac_data function")
+	my_iac_stub = Iac_Stub()
+	data = Iac_Stub.get_iac_stub(my_iac_stub)
+	return data
+
+if __name__ == '__main__':
+	app.run(host='localhost', port=8121)
---
+++ iac_stub.py
@@ -0,0 +1,17 @@
+import logging
+
+from structlog import wrap_logger
+from structlog import PrintLogger #added by EC
+
+logger = wrap_logger(logging.getLogger("iac-stub"))
+
+class Iac_Stub:
+
+    def __init__(self):
+        self.iac_fixed_response = None
+
+    def get_iac_stub(self):
+        PrintLogger().info("Now in the get_iac_stub function")
+        iac_fixed_response = '{"caseId":"0337c579-ce9d-4357-a620-5e4c565cfac1","caseRef":"1000000000000002","iac":"b4t7g3xby5bx","active":true,"questionSet":null,"lastUsedDateTime":1542200512691}'
+        PrintLogger().info("Now returning the iac fixed response: " + repr(iac_fixed_response))
+        return iac_fixed_response

d9f45fc4e723c272a0b353db2f806a0a3307cef7
cookel2 <Eleanor.Cook@ons.gov.uk>
2018-11-15 14:35:36 +0000

    amended iac_stub.py to return a String but need to find out how to convert it to JSON

--- iac_stub.py
+++ iac_stub.py
@@ -12,6 +12,6 @@ class Iac_Stub:
 
     def get_iac_stub(self):
         PrintLogger().info("Now in the get_iac_stub function")
-        iac_fixed_response = {"caseId": "0337c579-ce9d-4357-a620-5e4c565cfac1", "caseRef": "1000000000000002", "iac": "b4t7g3xby5bx","active": True, "questionSet": None, "lastUsedDateTime": 1542287779444}
+        iac_fixed_response = '{"caseId": "0337c579-ce9d-4357-a620-5e4c565cfac1", "caseRef": "1000000000000002", "iac": "b4t7g3xby5bx","active": True, "questionSet": None, "lastUsedDateTime": 1542287779444}'
         PrintLogger().info("Now returning the iac fixed response: " + repr(iac_fixed_response))
         return iac_fixed_response

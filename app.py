df139531ebd3ab44995e2f8b10e23692e003936a
cookel2 <Eleanor.Cook@ons.gov.uk>
2018-11-22 11:51:46 +0000

    started creating case stub - functionality not yet complete

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import json
 from structlog import PrintLogger  # added by EC
 
 from flask import Flask
-from iac_stub import Iac_Stub
 from flask import Response
+from iac_stub import Iac_Stub
+from case_stub import Case_Stub
 
 app = Flask(__name__)
 
@@ -22,15 +23,17 @@ def show_iac_data():
     PrintLogger().info("Data returned successfully")
     return Response(json.dumps(data, separators=(',', ':')), mimetype='application/json')
 
-# @app.route('/iacs/b4t7g3xby5bx')
-# def stub_iac_data():
-# 	PrintLogger().info("Now in the stub_iac_data function")
-# 	iac_fixed_response = {'caseId': '0337c579-ce9d-4357-a620-5e4c565cfac1',
-# 						  'caseRef': '1000000000000002',
-# 						  'iac': 'b4t7g3xby5bx',
-# 						  'active': True}
-# 	PrintLogger().info("Nearly through the stub_iac_data function")
-# 	return json.dumps(iac_fixed_response)
+@app.route('/cases/0337c579-ce9d-4357-a620-5e4c565cfac1')
+def show_case_data():
+    PrintLogger().info("Now in the show_case_data function")
+    my_case_stub = Case_Stub()
+    data = Case_Stub.get_case_stub(my_case_stub)
+    PrintLogger().info("Data returned successfully")
+    return Response(json.dumps(data, separators=(',', ':')), mimetype='application/json')
 
+
+"""The port below needs to be changed depending on which stub we're using, as follows:
+iac is port 8121, 
+case is port 8171"""
 if __name__ == '__main__':
     app.run(host='localhost', port=8121)

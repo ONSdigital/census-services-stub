0e6bd8b45b97db3e93e18e2a5f7a256d35d8e82b
cookel2 <Eleanor.Cook@ons.gov.uk>
2019-01-04 13:06:32 +0000

    amend stub so that it only accepts iac codes vvvvvvvvvvvv or wwwwwwwwwwww

--- rm-services-rh-stub/iac_stub.py
+++ rm-services-rh-stub/iac_stub.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from uuid import uuid4
 
 class Iac_Stub:
@@ -9,9 +10,22 @@ class Iac_Stub:
 
     def get_iac_stub_for_all(self):
         new_case_id = str(uuid4())
-        iac_fixed_response = {"caseId": new_case_id, "caseRef": "1000000000000001", "iac": self.iac, "active": True,
-                              "questionSet": None, "lastUsedDateTime": 1542361992207}
+        if (self.iac == 'vvvvvvvvvvvv') or (self.iac == 'wwwwwwwwwwww'):
+            iac_fixed_response = {"caseId": new_case_id, "caseRef": "1000000000000001", "iac": self.iac, "active": True,
+                          "questionSet": None, "lastUsedDateTime": 1542361992207}
+        else:
+            logging.info("The iac code is not a valid one. It needs to be either vvvvvvvvvvvv or wwwwwwwwwwww")
 
         iac_json = json.dumps(iac_fixed_response)
 
         return iac_json
+
+
+    # def get_iac_stub_for_all(self):
+    #     new_case_id = str(uuid4())
+    #     iac_fixed_response = {"caseId": new_case_id, "caseRef": "1000000000000001", "iac": self.iac, "active": True,
+    #                           "questionSet": None, "lastUsedDateTime": 1542361992207}
+    #
+    #     iac_json = json.dumps(iac_fixed_response)
+    #
+    #     return iac_json
